6e8666415767b93c25a619ad58c7bb8096ad30cf
rajvaishnavi15 <148607924+rajvaishnavi15@users.noreply.github.com>
2024-08-01 18:31:26 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  role: { type: String, required: true, enum: ['admin', 'content', 'creator', 'user'] },
+  userType: { type: String, enum: ['buyer', 'tenant', 'owner'] },
+  firstSchool: { type: String, required: true }
 
 module.exports = mongoose.model("User", UserSchema);
